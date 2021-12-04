1b05d49439d383d520fcba6095c666d39215c7e2
Gonzalo FU <GFU-code@protonmail.ch>
2021-12-04 17:36:10 -0300

    cap02 - valores signed y unsigned (con o sin signo), ejemplos de uso

---
+++ cap 02 - Basic of C++ programming/signedIntegers.cpp
@@ -0,0 +1,32 @@
+/*********************************************************************
+ * Finding the balance of an account after three transactions        *
+ *********************************************************************/
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    // Definition of variables
+    int balance = 0;
+    int transaction;
+
+    // Inputting first transaction and adjusting the balance
+    cout << "Enter the value of the first transaction: ";
+    cin >> transaction;
+    balance = balance + transaction;
+
+    // Inputting second transaction and adjusting the balance
+    cout << "Enter the value of the second transaction: ";
+    cin >> transaction;
+    balance = balance + transaction;
+
+    // Inputting third transaction and adjusting the balance
+    cout << "Enter the value of the third transaction: ";
+    cin >> transaction;
+    balance = balance + transaction;
+
+    // Outputting the final balance
+    cout << "The total balance is: " << balance << " dollars";
+    
+    return 0;
+}
