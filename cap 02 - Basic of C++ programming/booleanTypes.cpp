93566b17a1783532707b9e7a45406639184034ce
Gonzalo FU <GFU-code@protonmail.ch>
2021-12-16 13:32:55 -0300

    cap02 - Revisando los tipos de datos boolean

---
+++ cap 02 - Basic of C++ programming/booleanTypes.cpp
@@ -0,0 +1,26 @@
+/******************************************************
+ * The use of Boolean variables and values            *
+ ******************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Variable definitions
+    bool x = 123;
+    bool y = -8;
+    bool z = 0;
+    bool t = -0;
+    bool u = true;
+    bool v = false;
+
+    // Mostrando los valores
+    cout << "Value of x: " << x << endl;
+    cout << "Value of y: " << y << endl;
+    cout << "Value of z: " << z << endl;
+    cout << "Value of t: " << t << endl;
+    cout << "Value of u: " << u << endl;
+    cout << "Value of v: " << v;
+    
+    return 0;
+}
