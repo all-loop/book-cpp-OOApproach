0796d7ffbafe077bd5fa815f454085d5dca4957b
Gonzalo FU <GFU-code@protonmail.ch>
2021-12-16 12:42:13 -0300

    cap 02 - operador sizeOf

---
+++ cap 02 - Basic of C++ programming/operatorSize.cpp
@@ -0,0 +1,12 @@
+/****************************************************************
+ * A Program to find the size of all three integer types        *
+ ****************************************************************/
+ #include <iostream>
+ using namespace std;
+
+int main() {
+    cout << "Size of short in is " << sizeof(short int) << " bytes." << endl;
+    cout << "Size of int is " << sizeof(int) << " bytes." << endl;
+    cout << "Size of long int is " << sizeof(long int) << " bytes." << endl;
+    return 0;
+}
