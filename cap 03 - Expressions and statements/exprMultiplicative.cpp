34462966fd2c58b0e2a0d2fec672d776c4146afa
Gonzalo FU <GFU-code@protonmail.ch>
2021-12-21 21:38:07 -0300

    cap03 - Expresiones multiplicativas: multiplo, division y resto

---
+++ cap 03 - Expressions and statements/exprMultiplicative.cpp
@@ -0,0 +1,26 @@
+/*******************************************************
+ * Shows effects of multiplicative expressions         *
+ *******************************************************/
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Multiplication
+    cout << "Testing multiplication operator" << endl;
+    cout << "Value of 3 * 4 = " << 3 * 4 << endl;
+    cout << "Value of 2.4 * 4.1 = " << 2.4 * 4.1 << endl;
+    cout << "Value of -3 * 4 = " << -3 * 4 << endl;
+
+    // Division
+    cout << "Testing division operator" << endl;
+    cout << "Value of 30 / 5 = " << 30 / 5 << endl;
+    cout << "Value of 4/7 = " << 4 / 7 << endl;
+
+    // Remainder 
+    cout << "Testing remainder operator" << endl;
+    cout << "Value of 30 % 5 = " << 30 % 5 << endl;
+    cout << "Value of 30 % 4 = " << 30 % 4 << endl;
+    cout << "Value of 3 % 7 = " << 3 % 7; 
+
+    return 0 ;
+}
