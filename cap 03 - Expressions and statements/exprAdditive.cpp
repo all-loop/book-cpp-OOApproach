dd1f45083e1baeeda745b08e1dc96c59add77584
Gonzalo FU <GFU-code@protonmail.ch>
2021-12-21 21:44:40 -0300

    cap03 - Expresiones aditivas

---
+++ cap 03 - Expressions and statements/exprAdditive.cpp
@@ -0,0 +1,19 @@
+/***********************************************************
+ * Shows effects of addition and substraction operators    *
+ ***********************************************************/
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Testing some add operations
+    cout << "Some addition operations" << endl;
+    cout << "Value of 30 + 5 = " << 30 + 5 << endl;
+    cout << "Value of 20.5 + 6.2 = " << 20.5 + 6.2 << endl;
+
+    // Testing some substract operations
+    cout << "Some substraction operations" << endl;
+    cout << "Value of 5 - 30 = " << 5 - 30 << endl;
+    cout << "Value of 51.2 - 30-4 = " << 51.2 - 30.4; 
+
+    return 0;
+}
