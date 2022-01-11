be9acc2d994aced92330872f37f82b5f23b94bcf
Gonzalo FU <GFU-code@protonmail.ch>
2022-01-11 12:04:53 -0300

    cap03 - probando el overflow y underflow de un tipo float

---
+++ cap 03 - Expressions and statements/underOverFlow-float.cpp
@@ -0,0 +1,26 @@
+/**********************************************************
+ * A program to test overflow and underflow in doubles.
+ **********************************************************/
+#include <iostream>
+#include <limits>
+using namespace std;
+
+int main() {
+    // Find the positive and negative maximum double
+    double num1 = +numeric_limits<double>::max();
+    double num2 = -numeric_limits<double>::max();
+
+    // Print the positive and negative maximum double
+    cout << "The value of maximum double: " << num1 << endl;
+    cout << "The value of minimum double: " << num2 << endl;
+
+    // Multiply the values by 1000.00
+    num1 *= 1000.00;
+    num2 *= 1000.00;
+
+    // Print the overflowed values
+    cout << "The value of num1 * 1000 after overflow: " << num1  << endl;
+    cout << "The value of num2 * 1000 after underflow: " << num2 << endl;
+
+    return 0;
+};
